f1cb149a83bf7398c880ff924b74685ab2d08a76
Dekkia <github@dekkia.de>
2024-12-26 16:19:37 +0100

    displaying dates in index based on posts

--- _builder/index.js
+++ _builder/index.js
@@ -11,38 +11,6 @@ posts.sort(function (a, b) {
     return new Date(b.createdAt) - new Date(a.createdAt);
 });
 
-//==== Build main index ====
-const mainIndexDom = new JSDOM('<!--THIS FILE HAS BEEN AUTOMATICALLY GENERATED PLEASE DO NOT MODIFY-->\n' + fs.readFileSync(path.join(__dirname, 'indexTemplate.html'), 'utf-8'));
-
-var LinkBox = mainIndexDom.window.document.getElementById('LinkBox');
-
-posts.forEach(blogPost => {
-
-    var linkContainer = mainIndexDom.window.document.createElement('a');
-    linkContainer.href = './post/' + blogPost.startPostID + '.html';
-    linkContainer.className = 'linkContainer';
-
-    var linkImage = mainIndexDom.window.document.createElement('img');
-    linkImage.src = 'postimg/' + blogPost.img;
-    linkImage.className = 'linkImage';
-    linkContainer.appendChild(linkImage);
-
-    var linkTitle = mainIndexDom.window.document.createElement('div');
-    linkTitle.innerHTML = blogPost.title;
-    linkTitle.className = 'linkTitle';
-    linkContainer.appendChild(linkTitle);
-
-    var linkDescription = mainIndexDom.window.document.createElement('div');
-    linkDescription.innerHTML = blogPost.description + ' Posted: ' + new Date(blogPost.createdAt).toLocaleDateString();
-    linkDescription.className = 'linkDescription';
-    linkContainer.appendChild(linkDescription);
-
-    LinkBox.appendChild(linkContainer);
-
-});
-
-fs.writeFileSync(path.join(__dirname, '../blog/index.html'), mainIndexDom.serialize(), { encoding: 'utf-8' });
-
 //==== Build single pages ====
 
 //Check if post-folder exists
@@ -56,13 +24,19 @@ if (!fs.existsSync(path.join(__dirname, '../blog/post/'))) {
         });
     });
 }
-
+var postcnt = 0;
 posts.forEach(blogPost => {
-    threadUnroll.initPageAsApi('https://dekkia.com', blogPost.startPostID, blogPost.title, function (out) {
+    threadUnroll.initPageAsApi('https://dekkia.com', blogPost.startPostID, blogPost.title, function (dom, arr) {
         const postDom = new JSDOM('<!--THIS FILE HAS BEEN AUTOMATICALLY GENERATED PLEASE DO NOT MODIFY-->\n' + fs.readFileSync(path.join(__dirname, 'postTemplate.html'), 'utf-8'));
-        out.className = "mainBody";
-        postDom.window.document.getElementById('mainPage').appendChild(out);
+        dom.className = "mainBody";
+        postDom.window.document.getElementById('mainPage').appendChild(dom);
         fs.writeFileSync(path.join(__dirname, '../blog/post/' + blogPost.startPostID + '.html'), postDom.serialize(), { encoding: 'utf-8' });
+
+        blogPost.oldestPost = arr[0].created_at;
+        blogPost.newestPost = arr[arr.length - 1].created_at;
+
+        postcnt++;
+        if (postcnt >= posts.length) makeIndex();
     }, false);
 
 });
@@ -70,3 +44,43 @@ posts.forEach(blogPost => {
 //Create index in blog-dir
 const postIndexDom = new JSDOM('<!--THIS FILE HAS BEEN AUTOMATICALLY GENERATED PLEASE DO NOT MODIFY-->\n' + fs.readFileSync(path.join(__dirname, 'postTemplate.html'), 'utf-8'));
 fs.writeFileSync(path.join(__dirname, '../blog/post/index.html'), postIndexDom.serialize(), { encoding: 'utf-8' });
+
+
+//==== Build main index ====
+
+function makeIndex() {
+
+    console.log(posts);
+
+    const mainIndexDom = new JSDOM('<!--THIS FILE HAS BEEN AUTOMATICALLY GENERATED PLEASE DO NOT MODIFY-->\n' + fs.readFileSync(path.join(__dirname, 'indexTemplate.html'), 'utf-8'));
+
+    var LinkBox = mainIndexDom.window.document.getElementById('LinkBox');
+
+    posts.forEach(blogPost => {
+
+        var linkContainer = mainIndexDom.window.document.createElement('a');
+        linkContainer.href = './post/' + blogPost.startPostID + '.html';
+        linkContainer.className = 'linkContainer';
+
+        var linkImage = mainIndexDom.window.document.createElement('img');
+        linkImage.src = 'postimg/' + blogPost.img;
+        linkImage.className = 'linkImage';
+        linkContainer.appendChild(linkImage);
+
+        var linkTitle = mainIndexDom.window.document.createElement('div');
+        linkTitle.innerHTML = blogPost.title;
+        linkTitle.className = 'linkTitle';
+        linkContainer.appendChild(linkTitle);
+
+        var linkDescription = mainIndexDom.window.document.createElement('div');
+        linkDescription.innerHTML = blogPost.description + '<br>Posted: ' + new Date(blogPost.oldestPost).toLocaleDateString() + ', Updated: ' + new Date(blogPost.newestPost).toLocaleDateString();
+        linkDescription.className = 'linkDescription';
+        linkContainer.appendChild(linkDescription);
+
+        LinkBox.appendChild(linkContainer);
+
+    });
+
+    fs.writeFileSync(path.join(__dirname, '../blog/index.html'), mainIndexDom.serialize(), { encoding: 'utf-8' });
+}
+
